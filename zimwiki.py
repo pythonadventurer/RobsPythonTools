266a5586ed827920c03ab88b4c05c04ccdaf68c3
Rob Fowler <robtf04@outlook.com>
2020-12-28 08:10:28 -0500

    updates to zim tools

--- zimwiki.py
+++ zimwiki.py
@@ -44,14 +44,33 @@ class ZimPage:
         find_title = re.search(re_title,self.content)
 
         self.title = find_title.group(1)
+        
+        # Strip the title out of the content
+        self.content = self.content[find_title.end()+1:]
 
         self.filename = Path(page).name
 
         self.parent = str(Path(page).parent)
 
 
-    def write_page(self):
+    def strip_newlines(self):
+        # eliminate newlines, except those between paragraphs
+        self.content = self.content.replace("\xa0","").split("\n")
 
+        new_content = ""
+
+        for line in self.content:
+            if line == "":
+                new_content += "\n\n"
+           
+            else:
+                new_content += line + " "
+
+        self.content = new_content
+
+
+    def write_page(self):
+        # Be sure to set .parent and .filename before calling write_page
         # File name is always derived from the title when writing page
         self.filename = self.title.replace(" ","_") + ".txt"
 
@@ -68,4 +87,9 @@ my_page.read_page(r"D:\OneDrive\Rob\_TO BE FILED\Lifeline\TXT\2018-01-01_Sarasot
 my_page.title = "2018-01-01-0000 Sarasota Conversations"
 
 my_page.parent = Path(config["zimwiki"]["notebook"],"2018")
+
+my_page.strip_newlines()
+
+my_page.write_page()
+
 
